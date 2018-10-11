d4d3fca3dd8424ac3fdfc9937eceb767b5165df9
andresflp <afpichamba@gmail.com>
2018-10-10 20:32:24 -0500

    acabamos modulo 7 creando pagina de items,buscador con filtro y carga de productos

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
+import { producto_Descripcion } from '../../interfaces/productoDescripcion.interface';
+import { InfoPaginaService } from '../../services/info-pagina.service';
+
 
 @Component({
   selector: 'app-item',
@@ -6,10 +11,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
-
-  constructor() { }
+  producto: producto_Descripcion;
+  id: string;
+  anio: number = new Date().getFullYear();
+  constructor(private route: ActivatedRoute,
+              public _productosService: ProductosService, public infoPaginaService: InfoPaginaService) { }
 
   ngOnInit() {
+    this.route.params.
+    subscribe(parametros =>{
+      //console.log(parametros['id']);
+     this._productosService.getProducto(parametros['id']).
+     subscribe((producto:producto_Descripcion) => { 
+      //guardamos nuestro objeto producto en una variable 
+      this.producto = producto;
+      this.id = parametros['id'];
+      //console.log(producto);
+     });
+
+    });
   }
 
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -11,6 +11,9 @@ export class ProductosService {
   //creamos una variable para guardar el arreglo de productos
   productos: Producto[] = [];
 
+  //creamos otra variable para guardar los filtrados por busqueda
+  productosFiltado: Producto[] = [];
+
   constructor(private http: HttpClient) { 
     this.cargarProductos ();
   }
@@ -18,15 +21,56 @@ export class ProductosService {
 
 
   private cargarProductos() {
-    this.http.get('https://angular-portafolio-e1804.firebaseio.com/productos_idx.json')
-    .subscribe((resp: Producto[]) => {
-      console.log (resp);
-      //ya esta cargada eso quiere decir que ya esta cargado la info
-      this.cargando = false;
-      
-      this.productos = resp;
+
+    //promesas
+    return new Promise( (resolve , reject) => {
+      this.http.get('https://angular-portafolio-e1804.firebaseio.com/productos_idx.json')
+      .subscribe((resp: Producto[]) => {
+       // console.log (resp);
+        //ya esta cargada eso quiere decir que ya esta cargado la info
+        this.cargando = false;
+        this.productos = resp;
+        resolve();
+      });
 
     });
+  }
 
+   getProducto(id: string){
+    return this.http.get(`https://angular-portafolio-e1804.firebaseio.com/productos/${id}.json`);
+  }
+
+  //metodo buscar
+  buscarProducto (termino: string){
+    if(this.productos.length === 0){
+      //cargar productos
+      this.cargarProductos().then( () => {
+        //todo este codigo se ejecuta despues de tener los productos cargados
+        //filtrar
+        this.filtrarProductos(termino);
+
+      });
+      
+    }
+    else{
+      //filtrar
+     this.filtrarProductos(termino);
+    }
+
+  }
+  private filtrarProductos(termino: string){
+      
+      this.productosFiltado = [];
+      termino = termino.toLocaleLowerCase();
+      console.log(termino);
+      this.productos.forEach( prod =>{
+        //como el titulo tiene mayusculas
+        //tenemos que cambiarlas a minusculas para evaluar
+        const tituloLower = prod.titulo.toLocaleLowerCase();
+
+        if (prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf ( termino ) >= 0 ) {
+         this.productosFiltado.push( prod );
+      }
+    });
   }
 }
